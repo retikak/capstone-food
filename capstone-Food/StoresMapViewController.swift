708cedf6e8f5becc0824ff40f0302eb59072df7b
retika <retika@Retikas-MacBook-Pro.local>
2016-09-17 17:44:18 -0600

    map finished

--- capstone-Food/StoresMapViewController.swift
+++ capstone-Food/StoresMapViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class StoresMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class StoresMapViewController: UIViewController, CLLocationManagerDelegate {
     
     
    // @IBOutlet weak var GroceryListTableView: UITableView!
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManger = CLLocationManager()
+    var resultSearchController: UISearchController? = nil
+    var selectedPin: MKPlacemark? = nil
     
-    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let seconds = Int64(1 * Double(NSEC_PER_SEC))
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, seconds), dispatch_get_main_queue()) {
+            self.resultSearchController?.searchBar.becomeFirstResponder()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +39,22 @@ class StoresMapViewController: UIViewController, MKMapViewDelegate, CLLocationMa
         locationManger.desiredAccuracy = kCLLocationAccuracyBest
         locationManger.requestWhenInUseAuthorization()
         locationManger.startUpdatingLocation()
+        
+        //searchBar
+        
+        let locationSearchTable = storyboard?.instantiateViewControllerWithIdentifier("LocationSearchTable") as? StoresTableViewController
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        let searchBar = resultSearchController?.searchBar
+        searchBar?.sizeToFit()
+        searchBar?.placeholder = "search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        locationSearchTable?.mapView = mapView
+        locationSearchTable?.handleMapSearchDelegate = self
+        
     }
     
     
@@ -51,9 +79,57 @@ class StoresMapViewController: UIViewController, MKMapViewDelegate, CLLocationMa
         
     }
     
+    func getDirections(){
+        if let selectedPin = selectedPin {
+            let mapItem = MKMapItem(placemark: selectedPin)
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            mapItem.openInMapsWithLaunchOptions(launchOptions)
+        }
+    }
     
-    
-    
+  }
+
+extension StoresMapViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension StoresMapViewController: MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView?.pinTintColor = UIColor.orangeColor()
+        pinView?.canShowCallout = true
+        let smallSquare = CGSize(width: 50, height: 50)
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+        button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+        button.setTitle("Go", forState: .Normal)
+        button.tintColor = UIColor.redColor()
+        button.titleLabel?.text = "Directions"
+        button.addTarget(self, action: "getDirections", forControlEvents: .TouchUpInside)
+        pinView?.leftCalloutAccessoryView = button
+        return pinView
+    }
 }
 
 
